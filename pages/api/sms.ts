c7c398fc20fd582dbbcf3e9b38f355ba5dfe9297
Georgi Yanev <georgi.yanev@elisa.fi>
2023-03-16 18:53:57 +0200

    Adjust sms route

--- pages/api/sms.ts
+++ pages/api/sms.ts
@@ -4,11 +4,16 @@ import bodyParser from 'body-parser'
 const app = require('express')()
 app.use(bodyParser.urlencoded({ extended: false }))
 
-export default async (req: NextApiRequest, res: NextApiResponse) => {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   if (req.method === 'POST') {
     const { Body, From } = req.body
     console.log(`Received SMS from ${From}: ${Body}`)
     // Process the incoming SMS message as needed
     res.status(200).end()
+  } else {
+    console.log('not a post request')
   }
 }
